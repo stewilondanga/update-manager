afe0d3507d660118f80d27b90a86648261cfe53c
Stewart ilondanga <stilondanga@gmail.com>
2018-07-02 17:38:30 +0300

    add app/src/main/res/layout/activity_main.xml

--- app/src/main/java/io/github/stewilondanga/update_manager/ui/MainActivity.java
+++ app/src/main/java/io/github/stewilondanga/update_manager/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package io.github.stewilondanga.update_manager;
+package io.github.stewilondanga.update_manager.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import io.github.stewilondanga.update_manager.R;
 
 public class MainActivity extends AppCompatActivity {
 
