0a938165c666510ac51bcbc016e5f824062400b1
Stewart ilondanga <stilondanga@gmail.com>
2018-07-02 18:03:26 +0300

    add app/src/main/res/layout/activity_update_detail.xml

---
+++ app/src/main/java/io/github/stewilondanga/update_manager/ui/UpdateDetailActivity.java
@@ -0,0 +1,15 @@
+package io.github.stewilondanga.update_manager.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.github.stewilondanga.update_manager.R;
+
+public class UpdateDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_detail);
+    }
+}
