5945b130261994a92d8a97ff5b94fc510f45d7eb
Stewart ilondanga <stilondanga@gmail.com>
2018-07-04 19:04:57 +0300

    add app/src/main/java/io/github/stewilondanga/update_manager/util/ItemTouchHelperViewHolder.java

---
+++ app/src/main/java/io/github/stewilondanga/update_manager/util/ItemTouchHelperViewHolder.java
@@ -0,0 +1,10 @@
+package io.github.stewilondanga.update_manager.util;
+
+/**
+ * Created by stewart on 7/4/18.
+ */
+
+public interface ItemTouchHelperViewHolder {
+    void onItemSelected();
+    void onItemClear();
+}
