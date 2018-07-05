3a09a9d29994e976d3060477c82a086f63a1751b
Stewart ilondanga <stilondanga@gmail.com>
2018-07-05 16:38:29 +0300

    add app/src/main/java/io/github/stewilondanga/update_manager/ui/UpdateDetailActivity.java

--- app/src/main/java/io/github/stewilondanga/update_manager/ui/UpdateDetailActivity.java
+++ app/src/main/java/io/github/stewilondanga/update_manager/ui/UpdateDetailActivity.java
@@ -1,15 +1,40 @@
 package io.github.stewilondanga.update_manager.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import io.github.stewilondanga.update_manager.Constants;
 import io.github.stewilondanga.update_manager.R;
+import io.github.stewilondanga.update_manager.adapters.UpdatePagerAdapter;
+import io.github.stewilondanga.update_manager.models.Update;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class UpdateDetailActivity extends AppCompatActivity {
+    @BindView(R.id.viewPager) ViewPager mViewPager;
+
+    private UpdatePagerAdapter adapterViewPager;
+    ArrayList<Update> mUpdates = new ArrayList<>();
+    private String mSource;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_update_detail);
+        ButterKnife.bind(this);
+
+        mUpdates = Parcels.unwrap(getIntent().getParcelableExtra("briefs"));
+        mSource = getIntent().getStringExtra(Constants.KEY_SOURCE);
+        int startingPosition = getIntent().getIntExtra("position", 0);
+
+        adapterViewPager = new UpdatePagerAdapter(getSupportFragmentManager(), mUpdates, mSource);
+        mViewPager.setAdapter(adapterViewPager);
+        mViewPager.setCurrentItem(startingPosition);
     }
 }
